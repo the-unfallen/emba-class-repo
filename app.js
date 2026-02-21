d68488b4978bbccc9bd9fba1a97b44977b6d1ac5
Ibrahim Ajani <saturnsteed@gmail.com>
2026-02-21 18:28:45 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,72 @@
+// Define your data
+const courseData = {
+    general: {
+        "All Case Studies":
+            "https://drive.google.com/drive/folders/GENERAL_CASE_STUDIES_LINK",
+        "Time Table": "https://drive.google.com/drive/folders/TIMETABLE_LINK",
+        "Exam Past Questions":
+            "https://drive.google.com/drive/folders/EXAM_PAST_QUESTIONS_LINK",
+    },
+    courses: [
+        {
+            name: "Course 1",
+            resources: {
+                "Case Studies":
+                    "https://drive.google.com/drive/folders/COURSE1_CASE_STUDIES_LINK",
+                "Learning Work Plan":
+                    "https://drive.google.com/drive/folders/COURSE1_LEARNING_WORK_PLAN_LINK",
+                Books: "https://drive.google.com/drive/folders/COURSE1_BOOKS_LINK",
+                "Lecture Slides":
+                    "https://drive.google.com/drive/folders/COURSE1_LECTURE_SLIDES_LINK",
+                Assignment:
+                    "https://drive.google.com/drive/folders/COURSE1_ASSIGNMENT_LINK",
+                "Past Questions":
+                    "https://drive.google.com/drive/folders/COURSE1_PAST_QUESTIONS_LINK",
+            },
+        },
+        {
+            name: "Course 2",
+            resources: {
+                "Case Studies": "#",
+                "Learning Work Plan": "#",
+                Books: "#",
+                "Lecture Slides": "#",
+                Assignment: "#",
+                "Past Questions": "#",
+            },
+        },
+        {
+            name: "Course 3",
+            resources: {
+                "Case Studies": "#",
+                "Learning Work Plan": "#",
+                Books: "#",
+                "Lecture Slides": "#",
+                Assignment: "#",
+                "Past Questions": "#",
+            },
+        },
+        {
+            name: "Course 4",
+            resources: {
+                "Case Studies": "#",
+                "Learning Work Plan": "#",
+                Books: "#",
+                "Lecture Slides": "#",
+                Assignment: "#",
+                "Past Questions": "#",
+            },
+        },
+        {
+            name: "Course 5",
+            resources: {
+                "Case Studies": "#",
+                "Learning Work Plan": "#",
+                Books: "#",
+                "Lecture Slides": "#",
+                Assignment: "#",
+                "Past Questions": "#",
+            },
+        },
+    ],
+};
